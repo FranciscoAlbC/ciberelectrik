fca12d79853f0b624fc1122bb05c601ba92affa0
Francisco Alberca <francisco.albercac@gmail.com>
2024-10-07 13:12:20 -0500

    Add registrar_marca

--- src/main/java/pe/com/ciberelectrik/controller/MarcaController.java
+++ src/main/java/pe/com/ciberelectrik/controller/MarcaController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.com.ciberelectrik.entity.MarcaEntity;
 import pe.com.ciberelectrik.service.MarcaService;
 
 @Controller
@@ -16,5 +19,21 @@ public class MarcaController {
     public String MostrarMarca(Model modelo) {
         modelo.addAttribute("marcas", servicio.findAllCustom());
         return "marca/listar_marca";
+    }
+
+    @GetMapping("/registromarca")
+    public String MostrarRegistroMarca() {
+        return "marca/registrar_marca";
+    }
+
+    @ModelAttribute("marca")
+    public MarcaEntity ModeloMarca() {
+        return new MarcaEntity();
+    }
+
+    @PostMapping("/registrarmarca")
+    public String RegistrarMarca(@ModelAttribute("marca") MarcaEntity m) {
+        servicio.add(m);
+        return "redirect:/listarmarca";
     }
 }
